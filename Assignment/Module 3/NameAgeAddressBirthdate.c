eb640ec11e0c2203ea3d2a6420949750e3d06788
parag12344321 <131583938+parag12344321@users.noreply.github.com>
2023-07-22 16:26:25 +0530

    Update NameAgeAddressBirthdate.c

--- Assignment/Module 3/NameAgeAddressBirthdate.c
+++ Assignment/Module 3/NameAgeAddressBirthdate.c
@@ -1,9 +1,9 @@
 //Display This Information using printf
 
 #include<stdio.h>
-void main()
+int main()
 {
-	printf("Parag Mehta");
+	printf("Name : Parag Mehta");
 	printf("\nBirth Date: 10/11/1985");
 	printf("\nAge - 37");
 	printf("\nAdress : 202, Vedmata Residency-1, Patel Colony 5/6, Jamnagar");
